fff3b9fbf31fefe095479ddd272b6f2b31e7a2db
Jason Hsu <jhsu802701@jasonhsu.com>
2019-05-09 21:25:51 +0000

    Added export commands to variables.sh

--- variables.sh
+++ variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ABBREV='min-stage1'
-OWNER='rubyonracetracks'
-BRANCH='buster'
-DISTRO='debian'
-DOCKER_IMAGE="registry.gitlab.com/$OWNER/docker-$DISTRO-$BRANCH-$ABBREV"
-DOCKER_CONTAINER="container-$ABBREV"
+export ABBREV='min-stage1'
+export OWNER='rubyonracetracks'
+export BRANCH='buster'
+export DISTRO='debian'
+export DOCKER_IMAGE="registry.gitlab.com/$OWNER/docker-$DISTRO-$BRANCH-$ABBREV"
+export DOCKER_CONTAINER="container-$ABBREV"
