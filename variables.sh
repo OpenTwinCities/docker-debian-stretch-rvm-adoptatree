300b27db4978ffc31a0c770e503905b89ba7a39a
Jason Hsu <jhsu802701@jasonhsu.com>
2019-05-10 16:32:25 +0000

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -2,7 +2,7 @@
 
 export ABBREV='min-stage1'
 export OWNER='rubyonracetracks'
-export BRANCH='buster'
+export SUITE='buster'
 export DISTRO='debian'
-export DOCKER_IMAGE="registry.gitlab.com/$OWNER/docker-$DISTRO-$BRANCH-$ABBREV"
+export DOCKER_IMAGE="registry.gitlab.com/$OWNER/docker-$DISTRO-$SUITE-$ABBREV"
 export DOCKER_CONTAINER="container-$ABBREV"
