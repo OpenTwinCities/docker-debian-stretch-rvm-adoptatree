6d60d9c8f8b8eb11d6fa60b44c3aa9d1ab8acf14
Jason Hsu <jhsu@core>
2019-05-09 19:12:45 -0500

    Removed argument from build-image.sh script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -O - https://gitlab.com/rubyonracetracks/docker-debian-common/raw/master/build-image.sh | bash -s "$DOCKER_IMAGE"
+
+wget -O - https://gitlab.com/rubyonracetracks/docker-debian-common/raw/master/build-image.sh | bash
