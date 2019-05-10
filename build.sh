401f72d02a2f6605587fe29e74a20fbb53648e34
Jason Hsu <jhsu@core>
2019-05-09 19:07:24 -0500

    Use common script for building image

--- build.sh
+++ build.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-source variables.sh
-
-echo '************************************'
-echo "Docker image to build: $DOCKER_IMAGE"
-echo '************************************'
-
-wget -O - https://gitlab.com/rubyonracetracks/docker-debian-common/raw/master/delete-containers.sh | bash -s "$DOCKER_CONTAINER"
-wget -O - https://gitlab.com/rubyonracetracks/docker-debian-common/raw/master/delete-images.sh | bash -s "$DOCKER_IMAGE"
-
-DATE=`date +%Y_%m%d_%H%M_%S`
-DIR_LOG=$PWD/log
-mkdir -p $DIR_LOG
-
-echo '****************************'
-echo "BEGIN building $DOCKER_IMAGE"
-echo '****************************'
-
-docker build -t $DOCKER_IMAGE . 2>&1 | tee $DIR_LOG/build-$DATE.txt
-
-echo '*******************************'
-echo "Finished building $DOCKER_IMAGE"
-echo '*******************************'
+wget -O - https://gitlab.com/rubyonracetracks/docker-debian-common/raw/master/build-image.sh | bash -s "$DOCKER_IMAGE"
