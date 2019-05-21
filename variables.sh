d81702a22065c9ca46ef271f61dcf03334d1918e
Jason Hsu <jhsu@core>
2019-05-21 18:23:06 -0500

    Updated DOCKER_CONTAINER

--- variables.sh
+++ variables.sh
@@ -5,4 +5,4 @@ export OWNER='rubyonracetracks'
 export SUITE='buster'
 export DISTRO='debian'
 export DOCKER_IMAGE="registry.gitlab.com/$OWNER/docker-$DISTRO-$SUITE-$ABBREV"
-export DOCKER_CONTAINER="container-$ABBREV"
+export DOCKER_CONTAINER="container-$DISTRO-$SUITE-$ABBREV"
