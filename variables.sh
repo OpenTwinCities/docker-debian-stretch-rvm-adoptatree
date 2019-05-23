7cdb0ac074c677e9070dc5391d22192451419d27
Jason Hsu <jhsu@core>
2019-05-23 11:42:17 -0500

    sessionizer-adoptatree

--- variables.sh
+++ variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export ABBREV='rvm-rails-sessionizer'
-export OWNER='jhsu802701'
+export ABBREV='rvm-rails-adoptatree'
+export OWNER='opentwincities'
 export SUITE='stretch'
 export DISTRO='debian'
-export DOCKER_IMAGE="registry.gitlab.com/$OWNER/docker-$DISTRO-$SUITE-$ABBREV"
+export DOCKER_IMAGE="$OWNER/$DISTRO-$SUITE-$ABBREV"
 export DOCKER_CONTAINER="container-$DISTRO-$SUITE-$ABBREV"
